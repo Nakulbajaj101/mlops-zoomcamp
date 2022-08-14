abe671ebfa32ef2c0da1915fae7c9b6092b4d0e0
Nakul Bajaj <bajaj.nakul@gmail.com>
2022-08-14 17:23:18 +1000

    updated the policy name as was same for stage and prod

--- 06-best-practices/code-practice/deploy/run_apply_local.sh
+++ 06-best-practices/code-practice/deploy/run_apply_local.sh
@@ -13,10 +13,12 @@ if [[ $ENV == "stage" ]]
     then 
     export STREAM_NAME="ride_predictions_${ENV}"
     export MODEL_BUCKET="mlops-zoomcamp-nakul-${ENV}"
+    export FUNCTION_NAME="ride-prediction-service-${ENV}"
 elif [[ $ENV == "prod" ]]
     then
     export STREAM_NAME="ride_predictions"
     export MODEL_BUCKET="mlops-zoomcamp-nakul"
+    export FUNCTION_NAME="ride-prediction-service"
 else 
     echo "Value must be either stage or prod"
     exit 1
@@ -35,4 +37,4 @@ aws s3 sync s3://${MODEL_BUCKET_DEV} s3://${MODEL_BUCKET}
 # update the environment variables for lambda function
 echo "Updating the lambda function"
 variables="{STREAM_NAME=${STREAM_NAME}, MODEL_BUCKET=${MODEL_BUCKET}, RUN_ID=${RUN_ID}, TEST_RUN=${TEST_RUN}}"
-aws --region us-east-2 lambda update-function-configuration --function-name ride-prediction-service-${ENV} --environment "Variables=${variables}"
+aws --region us-east-2 lambda update-function-configuration --function-name $FUNCTION_NAME --environment "Variables=${variables}"
